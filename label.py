2ed1f58b4021e5294ae3a1c60184028b2c7be86e
albashzia <ahmedalbashzia@gmail.com>
2026-04-22 23:11:26 +0500

    Setting the font type, size and style of the text for the label

--- label.py
+++ label.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window = Tk()
 window.geometry("300x300")
-label = Label(window,text="Hello World")
+label = Label(window,text="Hello World",font=('Arial',20,'bold'))
 label.place(x=0,y=0)
 #label.pack()
 
