aeb7e3783c73b55ebcaff5c1ca771ce368598572
albashzia <ahmedalbashzia@gmail.com>
2026-04-22 23:14:28 +0500

    Changing the background color of the label to black using "bg"

--- label.py
+++ label.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window = Tk()
 window.geometry("300x300")
-label = Label(window,text="Hello World",font=('Arial',20,'bold'),fg='green')
+label = Label(window,text="Hello World",font=('Arial',20,'bold'),fg='green',bg='black')
 label.place(x=0,y=0)
 #label.pack()
 
