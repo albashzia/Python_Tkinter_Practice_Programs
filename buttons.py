d206f724fba28e3ee49a23f0eb8b7eba1c35c783
albashzia <ahmedalbashzia@gmail.com>
2026-04-23 15:25:47 +0500

    Defining a function named click to call on the button press

--- buttons.py
+++ buttons.py
@@ -1,12 +1,16 @@
 from tkinter import *
 
+def click():
+    print("You clicked the button!")
+
 window = Tk()
 
 window.geometry("300x200")
 window.title("Buttons")
 
 button = Button(window,
-                text="Click Me!")
+                text="Click Me!",
+                command=click)
 button.pack()
 
 window.mainloop()
