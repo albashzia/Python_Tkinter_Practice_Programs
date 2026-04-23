97b4a2ccfcd2baa9b4b194c4694ce0411be448ed
albashzia <ahmedalbashzia@gmail.com>
2026-04-23 15:29:52 +0500

    Changing the foreground, background, active foreground and active background color for the button

--- buttons.py
+++ buttons.py
@@ -8,10 +8,16 @@ window = Tk()
 window.geometry("300x200")
 window.title("Buttons")
 
-button = Button(window,
+button = Button(
+                window,
                 text="Click Me!",
                 command=click,
-                font=("Comic Sans",30))
+                font=("Comic Sans",30),
+                fg='green',
+                bg='black',
+                activeforeground='green',
+                activebackground='black'
+                )
 button.pack()
 
 window.mainloop()
