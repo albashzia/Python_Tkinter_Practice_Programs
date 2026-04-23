c02479b1edadf77a9f3c3f4f54029fd1bad95163
albashzia <ahmedalbashzia@gmail.com>
2026-04-23 15:27:10 +0500

    Changing the font type and size of the text on the button

--- buttons.py
+++ buttons.py
@@ -10,7 +10,8 @@ window.title("Buttons")
 
 button = Button(window,
                 text="Click Me!",
-                command=click)
+                command=click,
+                font=("Comic Sans",30))
 button.pack()
 
 window.mainloop()
