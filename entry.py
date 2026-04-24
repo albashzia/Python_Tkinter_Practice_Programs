09f99a1d388267a112888a4b718ca65e0b9ad782
albashzia <ahmedalbashzia@gmail.com>
2026-04-24 09:04:58 +0500

    Creating a button named "submit_button" and adding it to the window

--- entry.py
+++ entry.py
@@ -6,4 +6,10 @@ entry = Entry(window,
               font=('Arial',50))
 entry.pack()
 
+
+submit_button = Button(window,
+                       text="Submit")
+
+submit_button.pack()
+
 window.mainloop()
