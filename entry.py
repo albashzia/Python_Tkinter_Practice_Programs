074fb1d635ce989f02a8571e8167c1357f569909
albashzia <ahmedalbashzia@gmail.com>
2026-04-24 09:49:32 +0500

    Defining a backspace() function and passing it as a command to the backspace_button object

--- entry.py
+++ entry.py
@@ -8,6 +8,9 @@ def submit():
 def delete():
     entry.delete(0,END)
 
+def backspace():
+    entry.delete(len(entry.get())-1,END)
+
 entry = Entry(window,
               font=('Arial',50))
 entry.pack(side=LEFT)
@@ -26,7 +29,8 @@ delete_button = Button(window,
 delete_button.pack(side=RIGHT)
 
 backspace_button = Button(window,
-                       text="Backspace")
+                       text="Backspace",
+                       command=backspace)
 
 backspace_button.pack(side=RIGHT)
 
