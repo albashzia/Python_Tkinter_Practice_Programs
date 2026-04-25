9d96c9fba74c026c4fe22a90ac691c0a9549209c
albashzia <ahmedalbashzia@gmail.com>
2026-04-25 20:30:37 +0500

    Adding the on and off value to the Checkbutton

--- checkbox.py
+++ checkbox.py
@@ -6,7 +6,9 @@ x = IntVar()
 
 check_button = Checkbutton(window,
                            text="I agree to something",
-                           variable=x)
+                           variable=x,
+                           onvalue=1,
+                           offvalue=0)
 
 
 check_button.pack()
