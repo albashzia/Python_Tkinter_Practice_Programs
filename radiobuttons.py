34ed42c9cbb7bb710ed83487f84d203371f3c7c7
albashzia <ahmedalbashzia@gmail.com>
2026-04-27 19:33:01 +0500

    Creating an IntVar variable "x" and assigning it as a variable to the constructor of radio_button

--- radiobuttons.py
+++ radiobuttons.py
@@ -4,9 +4,12 @@ food = ["pizza","hamburger","hotdog"]
 
 window = Tk()
 
+x = IntVar()
+
 for index in range(len(food)):
-    radio_button = Radiobutton(window,
-                               text=food[index])
+    radio_button = Radiobutton(window, 
+                               text=food[index],
+                               variable=x)
     radio_button.pack()
 
 window.mainloop()
