20fb8663c970a7447f66788647213cbc18673625
albashzia <ahmedalbashzia@gmail.com>
2026-04-27 20:55:24 +0500

    Defining a function named order and using it as a command in radio_button's constructor

--- radiobuttons.py
+++ radiobuttons.py
@@ -13,6 +13,14 @@ foodImages = [pizzaImage,burgerImage,hotdogImage]
 
 x = IntVar()
 
+def order():
+    if(x.get()==0):
+        print("You ordered pizza")
+    elif (x.get()==1):
+        print("You ordered burger")
+    elif (x.get()==2);
+    print("You ordered hotdog")
+
 for index in range(len(food)):
     radio_button = Radiobutton(window,
                                text=food[index],
