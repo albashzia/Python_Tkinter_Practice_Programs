b240b1d41e2bd00e6a8459c2e33612cea5e1f347
albashzia <ahmedalbashzia@gmail.com>
2026-04-27 19:27:58 +0500

    Running a loop on "food" list, creating a radio button for each of its element, setting the text of radio button to the element name of list and packing the radio_button to make the radio buttons appear on the screen

--- radiobuttons.py
+++ radiobuttons.py
@@ -4,6 +4,9 @@ food = ["pizza","hamburger","hotdog"]
 
 window = Tk()
 
-
+for index in range(len(food)):
+    radio_button = Radiobutton(window,
+                               text=food[index])
+    radio_button.pack()
 
 window.mainloop()
