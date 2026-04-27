c80461160ff6ca5aa932c763cf8ab7561e746158
albashzia <ahmedalbashzia@gmail.com>
2026-04-27 19:37:19 +0500

    Adding the padding on x and y axis

--- radiobuttons.py
+++ radiobuttons.py
@@ -10,7 +10,9 @@ for index in range(len(food)):
     radio_button = Radiobutton(window,
                                text=food[index],
                                variable=x,
-                               value=index)
+                               value=index,
+                               padx=25,
+                               pady=15)
     radio_button.pack(anchor=W)
 
 window.mainloop()
