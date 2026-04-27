8bb450d95da9e9b79e5f211769497b71c86e3e39
albashzia <ahmedalbashzia@gmail.com>
2026-04-27 19:33:41 +0500

    Assigning the value to each radio button same as the value of index

--- radiobuttons.py
+++ radiobuttons.py
@@ -7,9 +7,10 @@ window = Tk()
 x = IntVar()
 
 for index in range(len(food)):
-    radio_button = Radiobutton(window, 
+    radio_button = Radiobutton(window,
                                text=food[index],
-                               variable=x)
+                               variable=x,
+                               value=index)
     radio_button.pack()
 
 window.mainloop()
