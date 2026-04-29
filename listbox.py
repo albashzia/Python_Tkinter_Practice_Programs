a557b7e3a291fbd6207465f825cb62c6f831d4f9
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 06:37:49 +0500

    Inserting elements in the listbox using the .insert() function, passing index and element as attributes.

--- listbox.py
+++ listbox.py
@@ -5,4 +5,10 @@ window = Tk()
 listbox = Listbox(window)
 listbox.pack()
 
+listbox.insert(0,"Pizza")
+listbox.insert(1,"Pasta")
+listbox.insert(2,"Garlic Bread")
+listbox.insert(3,"Soup")
+listbox.insert(4,"Salad")
+
 window.mainloop()
