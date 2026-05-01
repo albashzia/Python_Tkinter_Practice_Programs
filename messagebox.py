dc249bca957949198b17cf76ce4d22476186cb6c
albashzia <ahmedalbashzia@gmail.com>
2026-05-01 20:00:32 +0500

    Defining a button, adding command and text and packing it to make it appear on the window

--- messagebox.py
+++ messagebox.py
@@ -5,7 +5,9 @@ window = Tk()
 window.title("Message Box")
 window.geometry("300x100")
 
-
-
+button = Button(window,
+                command=click,
+                text="Click Me")
+button.pack()
 
 window.mainloop()
