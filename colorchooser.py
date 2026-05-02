c3b9a06900aa7a4ee6a6123ffb5dbd9f2fbf6d31
albashzia <ahmedalbashzia@gmail.com>
2026-05-02 12:11:56 +0500

    Defining a function named click and passing it as a command to the button

--- colorchooser.py
+++ colorchooser.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from tkinter import colorchooser
-
+def click():
+    print("Hello")
 window = Tk()
 window.geometry("420x420")
 
-button = Button(window, text="Click Here")
+button = Button(window, text="Click Here",command=click)
 button.pack()
 window.mainloop()
