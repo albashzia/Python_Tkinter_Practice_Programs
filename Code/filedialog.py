ed0f35b2f8b5b522f80412638f7184947c0892cd
albashzia <ahmedalbashzia@gmail.com>
2026-05-03 13:52:19 +0500

    Defining an open_file function to use file dialog to get the path of a file, opening the file, reading the file and then closing the file

--- Code/filedialog.py
+++ Code/filedialog.py
@@ -1,5 +1,10 @@
 from tkinter import *
 from tkinter import filedialog
+def open_file():
+    file_path = filedialog.askopenfilename()
+    file = open(file_path,'r')
+    print(file.read())
+    file.close()
 
 window = Tk()
 window.geometry("150x100")
