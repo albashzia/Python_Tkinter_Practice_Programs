c4fc459fa2dd860c217f7bb65d87d5fdd331c53e
albashzia <ahmedalbashzia@gmail.com>
2026-05-03 13:50:29 +0500

    Defining the geometry of window and creating a button, added to the window

--- Code/filedialog.py
+++ Code/filedialog.py
@@ -2,6 +2,9 @@ from tkinter import *
 from tkinter import filedialog
 
 window = Tk()
+window.geometry("150x100")
+button = Button(window,
+                text="Open File")
 
-
+button.pack()
 window.mainloop()
