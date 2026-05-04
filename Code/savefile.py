613c46892665be2928f95cd7b81296058b3504a2
albashzia <ahmedalbashzia@gmail.com>
2026-05-04 21:47:14 +0500

    Defining a function to ask to save as file and passing that function's name as command to button

--- Code/savefile.py
+++ Code/savefile.py
@@ -1,13 +1,17 @@
 from tkinter import *
 from tkinter import filedialog
 
+def save_file():
+    file = filedialog.asksaveasfile()
+
 window = Tk()
 
 text = Text(window)
 text.pack()
 
 button = Button(window,
-                text="Save")
+                text="Save",
+                command=save_file)
 button.pack()
 
 window.mainloop()
