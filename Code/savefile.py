4fb8461f93a96692cd7e60f8bcad3f4c8949697f
albashzia <ahmedalbashzia@gmail.com>
2026-05-04 21:49:21 +0500

    Setting the default extension of the file to ".txt"

--- Code/savefile.py
+++ Code/savefile.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import filedialog
 
 def save_file():
-    file = filedialog.asksaveasfile()
+    file = filedialog.asksaveasfile(defaultextension=".txt")
     file_text = text.get("1.0",END)
     file.write(file_text)
     file.close()
