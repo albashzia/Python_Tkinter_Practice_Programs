9bc7d39c64b0dbeb90c6993cdbd0fce0fe05f94c
albashzia <ahmedalbashzia@gmail.com>
2026-05-05 21:18:35 +0500

    Creating commands named "Open","Save" and "Exit" to file_menu

--- Code/menubar.py
+++ Code/menubar.py
@@ -7,5 +7,8 @@ window.config(menu=menubar)
 
 fileMenu = Menu(menubar)
 menubar.add_cascade(label="File",menu=fileMenu)
+fileMenu.add_command(label="Open")
+fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Exit")
 
 window.mainloop()
