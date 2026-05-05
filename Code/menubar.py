b01cd36ddb768ce299abc544603c385a06913fe1
albashzia <ahmedalbashzia@gmail.com>
2026-05-05 21:27:15 +0500

    Creating a function named "save" and adding it as a command to filemenu for "Save" label

--- Code/menubar.py
+++ Code/menubar.py
@@ -4,13 +4,16 @@ window = Tk()
 def open():
     print("You opened a file")
 
+def save():
+    print("You saved a file")
+
 menubar = Menu(window)
 window.config(menu=menubar)
 
 fileMenu = Menu(menubar)
 menubar.add_cascade(label="File",menu=fileMenu)
 fileMenu.add_command(label="Open", command=open)
-fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Save",command=save)
 fileMenu.add_command(label="Exit")
 
 window.mainloop()
