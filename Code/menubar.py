11d77c1863c52f18eecb4ca5ee46d22bce0c862c
albashzia <ahmedalbashzia@gmail.com>
2026-05-05 21:26:29 +0500

    Creating a function named "Open" and adding it as a command to filemenu for "Open" label

--- Code/menubar.py
+++ Code/menubar.py
@@ -1,13 +1,15 @@
 from tkinter import *
 
 window = Tk()
+def open():
+    print("You opened a file")
 
 menubar = Menu(window)
 window.config(menu=menubar)
 
 fileMenu = Menu(menubar)
 menubar.add_cascade(label="File",menu=fileMenu)
-fileMenu.add_command(label="Open")
+fileMenu.add_command(label="Open", command=open)
 fileMenu.add_command(label="Save")
 fileMenu.add_command(label="Exit")
 
