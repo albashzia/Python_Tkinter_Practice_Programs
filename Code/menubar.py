6c26657d8922a3a6f962e3c469d20a448d28ed2b
albashzia <ahmedalbashzia@gmail.com>
2026-05-05 21:27:51 +0500

    Adding command for the exit menu on filemenu

--- Code/menubar.py
+++ Code/menubar.py
@@ -14,6 +14,6 @@ fileMenu = Menu(menubar)
 menubar.add_cascade(label="File",menu=fileMenu)
 fileMenu.add_command(label="Open", command=open)
 fileMenu.add_command(label="Save",command=save)
-fileMenu.add_command(label="Exit")
+fileMenu.add_command(label="Exit", command=quit)
 
 window.mainloop()
