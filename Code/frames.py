db1f47a57e38beb4a6de2ff2558302624de4dc75
albashzia <ahmedalbashzia@gmail.com>
2026-05-07 21:03:00 +0500

    Creating 4 buttons, defining window as the container tag, specifying the font and width of the buttons

--- Code/frames.py
+++ Code/frames.py
@@ -2,5 +2,10 @@ from tkinter import *
 
 window = Tk()
 
+Button(window,text="W",font=("Consolas",25),width=3).pack()
+Button(window,text="A",font=("Consolas",25),width=3).pack()
+Button(window,text="S",font=("Consolas",25),width=3).pack()
+Button(window,text="D",font=("Consolas",25),width=3).pack()
+
 
 window.mainloop()
