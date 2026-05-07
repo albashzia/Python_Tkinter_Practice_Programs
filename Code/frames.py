d4071915f5fe99e030ab1fd06ad31d91c136a973
albashzia <ahmedalbashzia@gmail.com>
2026-05-07 21:04:05 +0500

    Specifying the sides of buttons in the pack functions associated with the buttons

--- Code/frames.py
+++ Code/frames.py
@@ -2,10 +2,10 @@ from tkinter import *
 
 window = Tk()
 
-Button(window,text="W",font=("Consolas",25),width=3).pack()
-Button(window,text="A",font=("Consolas",25),width=3).pack()
-Button(window,text="S",font=("Consolas",25),width=3).pack()
-Button(window,text="D",font=("Consolas",25),width=3).pack()
+Button(window,text="W",font=("Consolas",25),width=3).pack(side=TOP)
+Button(window,text="A",font=("Consolas",25),width=3).pack(side=LEFT)
+Button(window,text="S",font=("Consolas",25),width=3).pack(side=LEFT)
+Button(window,text="D",font=("Consolas",25),width=3).pack(side=LEFT)
 
 
 window.mainloop()
