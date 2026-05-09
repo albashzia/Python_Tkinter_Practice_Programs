dbcc3eae0d3d5b4e65c2013f59fe030ab77bb4bf
albashzia <ahmedalbashzia@gmail.com>
2026-05-09 22:01:43 +0500

    Setting the expansion of notebook to true and fill to both x and y axis

--- Code/window_tabs.py
+++ Code/window_tabs.py
@@ -9,7 +9,7 @@ tab1 = Frame(notebook)
 tab2 = Frame(notebook)
 notebook.add(tab1,text="Tab 1")
 notebook.add(tab2,text="Tab 2")
-notebook.pack()
+notebook.pack(expand=True,fill=BOTH)
 
 Label(tab1,text="Tab 1", width=50,height=25).pack()
 Label(tab2,text="Tab 2", width=50,height=25).pack()
