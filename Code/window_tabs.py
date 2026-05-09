04dbf8866c418fc418b625d3e44973073ab5d2c3
albashzia <ahmedalbashzia@gmail.com>
2026-05-09 21:55:58 +0500

    Creating frames and adding them to the notebooks

--- Code/window_tabs.py
+++ Code/window_tabs.py
@@ -5,4 +5,10 @@ window = Tk()
 
 notebook = ttk.Notebook(window)
 
+tab1 = Frame(notebook)
+tab2 = Frame(notebook)
+notebook.add(tab1,text="Tab 1")
+notebook.add(tab2,text="Tab 2")
+notebook.pack()
+
 window.mainloop()
