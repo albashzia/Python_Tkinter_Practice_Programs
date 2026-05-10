2e513bbb9296d769c49b8d1c1e15196856dda318
albashzia <ahmedalbashzia@gmail.com>
2026-05-10 22:43:20 +0500

    Creating another label and entry object for email  and using grid to organize them underneath the previously defined labels and entries

--- Code/grid_geometry_manager.py
+++ Code/grid_geometry_manager.py
@@ -8,4 +8,7 @@ first_name_entry = Entry(window).grid(row=0,column=1)
 last_name_label = Label(window,text="Last Name: ").grid(row=1,column=0)
 last_name_entry = Entry(window).grid(row=1,column=1)
 
+email_label = Label(window,text="Email: ").grid(row=2,column=0)
+email_entry = Entry(window).grid(row=2,column=1)
+
 window.mainloop()
