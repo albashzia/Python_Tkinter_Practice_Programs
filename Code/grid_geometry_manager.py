80174968059446db7411160f5f2881d1e0844da6
albashzia <ahmedalbashzia@gmail.com>
2026-05-10 22:41:24 +0500

    Creating another label and entry object and using grid to organize them underneath the previously defined label and entry

--- Code/grid_geometry_manager.py
+++ Code/grid_geometry_manager.py
@@ -5,4 +5,7 @@ window.geometry("220x220")
 first_name_label = Label(window,text="First Name: ").grid(row=0,column=0)
 first_name_entry = Entry(window).grid(row=0,column=1)
 
+last_name_label = Label(window,text="Last Name: ").grid(row=1,column=0)
+last_name_entry = Entry(window).grid(row=1,column=1)
+
 window.mainloop()
