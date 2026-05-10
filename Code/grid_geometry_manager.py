1208d4043008277edb63338dee0b50461c98baca
albashzia <ahmedalbashzia@gmail.com>
2026-05-10 22:57:27 +0500

    Creating a label, adding it to the top, moving all the rest elements down by 1 row

--- Code/grid_geometry_manager.py
+++ Code/grid_geometry_manager.py
@@ -2,15 +2,17 @@ from tkinter import *
 
 window = Tk()
 window.geometry("220x220")
-first_name_label = Label(window,text="First Name: ").grid(row=0,column=0)
-first_name_entry = Entry(window).grid(row=0,column=1)
+title_label = Label(window,text="Enter your info",font=("Arial",20)).grid(row=0,column=0,columnspan=2)
 
-last_name_label = Label(window,text="Last Name: ").grid(row=1,column=0)
-last_name_entry = Entry(window).grid(row=1,column=1)
+first_name_label = Label(window,text="First Name: ").grid(row=1,column=0)
+first_name_entry = Entry(window).grid(row=1,column=1)
 
-email_label = Label(window,text="Email: ").grid(row=2,column=0)
-email_entry = Entry(window).grid(row=2,column=1)
+last_name_label = Label(window,text="Last Name: ").grid(row=2,column=0)
+last_name_entry = Entry(window).grid(row=2,column=1)
 
-submit_button = Button(window,text="Submit").grid(row=3,column=0,columnspan=2)
+email_label = Label(window,text="Email: ").grid(row=3,column=0)
+email_entry = Entry(window).grid(row=3,column=1)
+
+submit_button = Button(window,text="Submit").grid(row=4,column=0,columnspan=2)
 
 window.mainloop()
