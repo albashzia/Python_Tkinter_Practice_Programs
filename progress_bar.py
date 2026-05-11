c176930450a11a931d31fa02a0a1bbda1b51c3ba
albashzia <ahmedalbashzia@gmail.com>
2026-05-11 21:00:30 +0500

    Renaming the tasks variable to task and setting the value for variable task in start function

--- progress_bar.py
+++ progress_bar.py
@@ -10,18 +10,19 @@ def start():
         bar['value']+=10
         x+=1
         percent.set(str(int(x/tasks)*100)+"%")
+        task.set(str(x)+"/"+str(tasks)+" tasks completed")
         window.update_idletasks()
 
 window = Tk()
 
 percent = StringVar()
-tasks = StringVar()
+task = StringVar()
 
 bar = Progressbar(window,orient=HORIZONTAL,length=300)
 bar.pack(padx=10,pady=10)
 
 percent_label = Label(window,textvariable=percent).pack()
-tasks_label = Label(window,textvariable=tasks).pack()
+tasks_label = Label(window,textvariable=task).pack()
 
 Button(window,text="Download", command=start).pack()
 
