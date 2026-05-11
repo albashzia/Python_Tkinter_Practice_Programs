ccdf3d3a267a72d291d36a0d237244401de03e0d
albashzia <ahmedalbashzia@gmail.com>
2026-05-11 20:47:42 +0500

    Defining a function named start to record progress in the progress bar

--- progress_bar.py
+++ progress_bar.py
@@ -2,6 +2,14 @@ from tkinter import *
 from tkinter.ttk import *
 import time
 
+def start():
+    tasks = 10
+    x = 0
+    while(x<tasks):
+        time.sleep(1)
+        bar['value']+=10
+        x+=1
+
 window = Tk()
 
 bar = Progressbar(window,orient=HORIZONTAL,length=300)
