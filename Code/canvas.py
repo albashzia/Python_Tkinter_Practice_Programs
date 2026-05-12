e1275c5b9cabf760b9a1aed91eb842e9628ae60a
albashzia <ahmedalbashzia@gmail.com>
2026-05-12 22:32:25 +0500

    Commenting the line that created the rectangle

--- Code/canvas.py
+++ Code/canvas.py
@@ -6,7 +6,7 @@ canvas = Canvas(window, height=500,width=500)
 # canvas.create_line(0,0,500,500, fill="blue",width=5)
 # canvas.create_line(0,500,500,0, fill="red",width=5)
 
-canvas.create_rectangle(100,100,300,300, fill='purple')
+# canvas.create_rectangle(100,100,300,300, fill='purple')
 
 canvas.pack()
 
