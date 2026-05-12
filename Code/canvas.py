d146ed8c402b1443957eeb4d2206d61b29b35974
albashzia <ahmedalbashzia@gmail.com>
2026-05-12 22:28:43 +0500

    Changing the color of the line and increasing the width

--- Code/canvas.py
+++ Code/canvas.py
@@ -3,7 +3,7 @@ from tkinter import *
 window = Tk()
 
 canvas = Canvas(window, height=500,width=500)
-canvas.create_line(0,0,500,500)
+canvas.create_line(0,0,500,500, fill="blue",width=5)
 
 canvas.pack()
 
