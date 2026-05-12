8cf4350adad364297d4001ca25b1435dfe2fe5dc
albashzia <ahmedalbashzia@gmail.com>
2026-05-12 22:30:46 +0500

    Commenting the code for creating line objects

--- Code/canvas.py
+++ Code/canvas.py
@@ -3,8 +3,8 @@ from tkinter import *
 window = Tk()
 
 canvas = Canvas(window, height=500,width=500)
-canvas.create_line(0,0,500,500, fill="blue",width=5)
-canvas.create_line(0,500,500,0, fill="red",width=5)
+# canvas.create_line(0,0,500,500, fill="blue",width=5)
+# canvas.create_line(0,500,500,0, fill="red",width=5)
 
 canvas.pack()
 
