dbfa51c0b875a0fa2b6298eb50594c0189a73b7a
albashzia <ahmedalbashzia@gmail.com>
2026-05-12 22:31:39 +0500

    Creating a rectangle using the canvas object's create_rectangle function

--- Code/canvas.py
+++ Code/canvas.py
@@ -6,6 +6,8 @@ canvas = Canvas(window, height=500,width=500)
 # canvas.create_line(0,0,500,500, fill="blue",width=5)
 # canvas.create_line(0,500,500,0, fill="red",width=5)
 
+canvas.create_rectangle(100,100,300,300)
+
 canvas.pack()
 
 window.mainloop()
