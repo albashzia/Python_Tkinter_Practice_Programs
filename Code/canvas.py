daab15d65ca3b7953e7beb83fa25a3353b68e00d
albashzia <ahmedalbashzia@gmail.com>
2026-05-12 22:35:53 +0500

    Creating a list to hold the points for the triangle and passing it to the create_polygon function

--- Code/canvas.py
+++ Code/canvas.py
@@ -8,7 +8,8 @@ canvas = Canvas(window, height=500,width=500)
 
 # canvas.create_rectangle(100,100,300,300, fill='purple')
 
-canvas.create_polygon(250,0,500,500,0,500,fill='yellow', outline='black', width=5)
+triangle_points = [250,0,500,500,0,500]
+canvas.create_polygon(triangle_points,fill='yellow', outline='black', width=5)
 canvas.pack()
 
 window.mainloop()
