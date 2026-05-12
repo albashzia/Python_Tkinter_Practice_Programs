8ed65765c142bcd0ac5d8545f3727e95eff753ca
albashzia <ahmedalbashzia@gmail.com>
2026-05-12 22:29:53 +0500

    Creating another line from the left bottom corner to right top corner

--- Code/canvas.py
+++ Code/canvas.py
@@ -4,6 +4,7 @@ window = Tk()
 
 canvas = Canvas(window, height=500,width=500)
 canvas.create_line(0,0,500,500, fill="blue",width=5)
+canvas.create_line(0,500,500,0, fill="red",width=5)
 
 canvas.pack()
 
