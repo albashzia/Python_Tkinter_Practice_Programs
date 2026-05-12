ef8bbefba9db2c4019bc5737d80b73ca45b681dc
albashzia <ahmedalbashzia@gmail.com>
2026-05-12 22:41:48 +0500

    Commenting the code for polygon

--- Code/canvas.py
+++ Code/canvas.py
@@ -8,8 +8,10 @@ canvas = Canvas(window, height=500,width=500)
 
 # canvas.create_rectangle(100,100,300,300, fill='purple')
 
-triangle_points = [250,0,500,500,0,500]
-canvas.create_polygon(triangle_points,fill='yellow', outline='black', width=5)
+# triangle_points = [250,0,500,500,0,500]
+# canvas.create_polygon(triangle_points,fill='yellow', outline='black', width=5)
+
+
 canvas.pack()
 
 window.mainloop()
