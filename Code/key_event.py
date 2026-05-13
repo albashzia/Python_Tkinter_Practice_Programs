643733245cf4d0eb770a00778d6895e38cfb5b50
albashzia <ahmedalbashzia@gmail.com>
2026-05-13 23:37:07 +0500

    Adding config function to the label to set the text on label to be same as the key pressed by the user

--- Code/key_event.py
+++ Code/key_event.py
@@ -1,12 +1,12 @@
 from tkinter import *
 def act(event):
-    print("You pressed a key")
+    label.config(text=event.keysym)
 
 window = Tk()
 
 window.bind("<Key>", act)
 
-label = Label(window,"Helvetica", 100)
+label = Label(window,font=("Helvetica", 100))
 label.pack()
 
 window.mainloop()
