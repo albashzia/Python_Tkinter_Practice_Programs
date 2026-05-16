a4e0dbd4a03e0b53b4c6fd65402a32b3a8bb9388
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 20:13:04 +0500

    Using Enter as an event to call the function when the cursor enters the window

--- Code/mouse_events.py
+++ Code/mouse_events.py
@@ -7,6 +7,7 @@ window = Tk()
 
 #window.bind("<Button-1>",act)
 #window.bind("<Button-3>",act)
-window.bind("<ButtonRelease>",act)
+#window.bind("<ButtonRelease>",act)
+window.bind("<Enter>",act)
 
 window.mainloop()
