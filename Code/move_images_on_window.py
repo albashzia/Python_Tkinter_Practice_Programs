6d3985eeab9dae2ca70cb1e7fa2797a31799bdc9
albashzia <ahmedalbashzia@gmail.com>
2026-05-21 22:38:56 +0500

    Modifying window geometry, creating a PhotoImage object, creating a label, adding the image to the label and placing the label on the window

--- Code/move_images_on_window.py
+++ Code/move_images_on_window.py
@@ -2,6 +2,9 @@ from tkinter import *
 
 window = Tk()
 
-window.geometry("500x500")
+window.geometry("300x300")
+carImage = PhotoImage(file="../Icons/racecar40px.png")
+label = Label(window,image=carImage)
+label.place(x=0,y=0)
 
 window.mainloop()
