593e1f58b7e0d03a6baddfb670d774b2ae9f4cbb
albashzia <ahmedalbashzia@gmail.com>
2026-05-22 16:39:21 +0500

    Binding a key with window and creating a move_up method to move the level up when "w" key is pressed

--- Code/move_images_on_window.py
+++ Code/move_images_on_window.py
@@ -1,10 +1,15 @@
 from tkinter import *
 
+def move_up(event):
+    label.place(x = label.winfo_x(),y= label.winfo_y()-10)
+
 window = Tk()
 
 window.geometry("300x300")
 carImage = PhotoImage(file="../Icons/racecar40px.png")
 label = Label(window,image=carImage)
 label.place(x=0,y=0)
+
+window.bind("<w>",move_up)
 
 window.mainloop()
