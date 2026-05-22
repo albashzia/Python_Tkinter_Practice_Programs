cd8f655ae0a428e48436e7234829adbda6093250
albashzia <ahmedalbashzia@gmail.com>
2026-05-22 16:41:20 +0500

    Creating a move_down method to move the label down when "s" key is pressed

--- Code/move_images_on_window.py
+++ Code/move_images_on_window.py
@@ -3,6 +3,9 @@ from tkinter import *
 def move_up(event):
     label.place(x = label.winfo_x(),y= label.winfo_y()-10)
 
+def move_down(event):
+    label.place(x = label.winfo_x(),y= label.winfo_y()+10)
+
 window = Tk()
 
 window.geometry("300x300")
@@ -11,5 +14,6 @@ label = Label(window,image=carImage)
 label.place(x=0,y=0)
 
 window.bind("<w>",move_up)
+window.bind("<s>",move_down)
 
 window.mainloop()
