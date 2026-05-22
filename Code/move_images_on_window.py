ae8567666c5058b0e1c9eb107fdeac21a0419c8c
albashzia <ahmedalbashzia@gmail.com>
2026-05-22 16:43:38 +0500

    Binding "d" and "a" keys with move right and move left actions

--- Code/move_images_on_window.py
+++ Code/move_images_on_window.py
@@ -6,6 +6,12 @@ def move_up(event):
 def move_down(event):
     label.place(x = label.winfo_x(),y= label.winfo_y()+10)
 
+def move_right(event):
+    label.place(x = label.winfo_x()+10,y= label.winfo_y())
+
+def move_left(event):
+    label.place(x = label.winfo_x()-10,y= label.winfo_y())
+
 window = Tk()
 
 window.geometry("300x300")
@@ -15,5 +21,7 @@ label.place(x=0,y=0)
 
 window.bind("<w>",move_up)
 window.bind("<s>",move_down)
+window.bind("<d>",move_right)
+window.bind("<a>",move_left)
 
 window.mainloop()
