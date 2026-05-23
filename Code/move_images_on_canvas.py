b9549456518976f29910df0aded3f53697434ae2
albashzia <ahmedalbashzia@gmail.com>
2026-05-23 20:14:04 +0500

    Binding keys to the window for movement operations

--- Code/move_images_on_canvas.py
+++ Code/move_images_on_canvas.py
@@ -2,6 +2,11 @@ from tkinter import *
 
 window = Tk()
 
+window.bind("<w>",move_up)
+window.bind("<s>",move_down)
+window.bind("<a>",move_left)
+window.bind("<d>",move_right)
+
 canvas = Canvas(window,width=500,height=500)
 canvas.pack()
 
