da1be24f0923dac7749ab6f29e72e512c703a457
albashzia <ahmedalbashzia@gmail.com>
2026-05-23 21:54:51 +0500

    Adding comments to window.py file to improve the readability and understandability of the code

--- Code/window.py
+++ Code/window.py
@@ -1,12 +1,21 @@
+#importing package
 from tkinter import *
 
+#instantiating a window
 window = Tk()
+
+#defining the size of the window
 window.geometry("500x500")
+
+#defining the title of the window
 window.title("First Frame ")
 
+#creating a photo icon and setting it as window's icon
 icon = PhotoImage(file='../Icons/pic.png')
 window.iconphoto(True,icon)
 
+#setting the background of the window to black
 window.config(background="black")
 
+#making the window visible
 window.mainloop()
