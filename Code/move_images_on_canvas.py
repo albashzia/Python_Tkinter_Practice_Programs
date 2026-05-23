8db40d7de97b12a8b7ac0ae3da5f6f28bb027dfd
albashzia <ahmedalbashzia@gmail.com>
2026-05-23 20:17:40 +0500

    Reducing the size of the canvas to 300 x 300 pixels

--- Code/move_images_on_canvas.py
+++ Code/move_images_on_canvas.py
@@ -20,7 +20,7 @@ window.bind("<s>",move_down)
 window.bind("<a>",move_left)
 window.bind("<d>",move_right)
 
-canvas = Canvas(window,width=500,height=500)
+canvas = Canvas(window,width=300,height=300)
 canvas.pack()
 
 carImage = PhotoImage(file="../Icons/racecar40px.png")
