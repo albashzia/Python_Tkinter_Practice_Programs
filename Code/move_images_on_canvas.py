0013001835b2f287eac44281fa8fc46732d3b940
albashzia <ahmedalbashzia@gmail.com>
2026-05-23 20:08:01 +0500

    Creating a PhotoImage object and adding it to the canvas

--- Code/move_images_on_canvas.py
+++ Code/move_images_on_canvas.py
@@ -3,5 +3,8 @@ from tkinter import *
 window = Tk()
 
 canvas = Canvas(window,width=500,height=500)
+canvas.pack()
 
+carImage = PhotoImage(file="../Icons/racecar40px.png")
+carPic = canvas.create_image(0,0,image=carImage,anchor=NW)
 window.mainloop()
