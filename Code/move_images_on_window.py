f4e0598f3659e1f98505aecff44e7f741bed5791
albashzia <ahmedalbashzia@gmail.com>
2026-05-23 22:02:53 +0500

    Adding comments to move_images_on_window.py  file to improve the readability and understandability of the code

--- Code/move_images_on_window.py
+++ Code/move_images_on_window.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+#defining the functions
 def move_up(event):
     label.place(x = label.winfo_x(),y= label.winfo_y()-10)
 
@@ -12,13 +13,16 @@ def move_right(event):
 def move_left(event):
     label.place(x = label.winfo_x()-10,y= label.winfo_y())
 
+#defining window object
 window = Tk()
 
+#creating icons and placing them on the window
 window.geometry("300x300")
 carImage = PhotoImage(file="../Icons/racecar40px.png")
 label = Label(window,image=carImage)
 label.place(x=0,y=0)
 
+#binding keys to the window
 window.bind("<w>",move_up)
 window.bind("<s>",move_down)
 window.bind("<d>",move_right)
