51a6f9c8231c2e2a82d0da1d509f3adab55c83f9
albashzia <ahmedalbashzia@gmail.com>
2026-05-25 21:13:51 +0500

    Using canvas.move() function to move the ufo icon image on the canvas as per the defined x and y velocity

--- Code/animations.py
+++ Code/animations.py
@@ -21,6 +21,7 @@ canvas_image = canvas.create_image(0,0,image=ufo_image,anchor=NW)
 while True:
     coordinates = canvas.coords(canvas_image)
     print(coordinates)
+    canvas.move(canvas_image,x_velocity,y_velocity)
     window.update()
     time.sleep(0.01)
 
