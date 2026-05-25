bcf77778455c68c1ebdd571e58c54adeb6205dc6
albashzia <ahmedalbashzia@gmail.com>
2026-05-25 21:08:55 +0500

    Defining a while loop for the animation to keep running and printing the current coordinates of the image within it

--- Code/animations.py
+++ Code/animations.py
@@ -15,4 +15,10 @@ canvas_background = canvas.create_image(0,0,image=background_image,anchor=NW)
 ufo_image = PhotoImage(file="../Icons/ufo_80px.png")
 canvas_image = canvas.create_image(0,0,image=ufo_image,anchor=NW)
 
+while True:
+    coordinates = canvas.coords(canvas_image)
+    print(coordinates)
+    window.update()
+    time.sleep(0.01)
+
 window.mainloop()
