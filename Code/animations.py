d0450534e8d11cc31e5182814e543247ad5af5f2
albashzia <ahmedalbashzia@gmail.com>
2026-05-25 21:06:14 +0500

    Adding the ufo image to the canvas by creating a PhotoImage for the icon and using canvas's create_image function

--- Code/animations.py
+++ Code/animations.py
@@ -12,5 +12,7 @@ canvas.pack()
 background_image = PhotoImage(file="../Icons/space_background_500px.png")
 canvas_background = canvas.create_image(0,0,image=background_image,anchor=NW)
 
+ufo_image = PhotoImage(file="../Icons/ufo_80px.png")
+canvas_image = canvas.create_image(0,0,image=ufo_image,anchor=NW)
 
 window.mainloop()
