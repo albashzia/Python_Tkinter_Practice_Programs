316a3f0e75718821bd85036ea0db90a160939de1
albashzia <ahmedalbashzia@gmail.com>
2026-05-25 20:56:24 +0500

    Creating a canvas, defining constant variables for width and height of the canvas and adding the canvas to the window

--- Code/animations.py
+++ Code/animations.py
@@ -1,14 +1,13 @@
 from tkinter import *
 import time
 
+WIDTH = 500
+HEIGHT = 500
+
 window = Tk()
 
-
-
-
-
-
-
+canvas = Canvas(window, width= WIDTH, height= HEIGHT)
+canvas.pack()
 
 
 window.mainloop()
