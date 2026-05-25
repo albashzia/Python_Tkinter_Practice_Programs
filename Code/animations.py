ff2ea5bda314b4254655099ec6330b2f4d9729af
albashzia <ahmedalbashzia@gmail.com>
2026-05-25 21:19:36 +0500

    Using conditionals to return the icon on the canvas once it reaches the borders of the canvas

--- Code/animations.py
+++ Code/animations.py
@@ -21,6 +21,12 @@ canvas_image = canvas.create_image(0,0,image=ufo_image,anchor=NW)
 while True:
     coordinates = canvas.coords(canvas_image)
     print(coordinates)
+    if (coordinates[0] >= WIDTH or coordinates[0] < 0):
+        x_velocity = -x_velocity
+
+    if (coordinates[1] >= HEIGHT or coordinates[1] < 0):
+        y_velocity = -y_velocity
+
     canvas.move(canvas_image,x_velocity,y_velocity)
     window.update()
     time.sleep(0.01)
