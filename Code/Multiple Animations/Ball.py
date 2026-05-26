6f6e7047239bc20c60a95589468289cba35b1bce
albashzia <ahmedalbashzia@gmail.com>
2026-05-26 13:18:30 +0500

    Defining the constructor of Ball class

---
+++ Code/Multiple Animations/Ball.py
@@ -0,0 +1,6 @@
+class Ball:
+    def __init__(self,canvas,x,y,diameter,xVelocity,yVelocity,color):
+        self.canvas = canvas
+        self.image = canvas.create_oval(x,y,diameter,diameter,fill=color)
+        self.xVelocity = xVelocity
+        self.yVelocity = yVelocity
