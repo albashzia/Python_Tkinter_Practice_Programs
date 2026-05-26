abee0ab2d3639c5bf172687b10a57c0d55e2b4f1
albashzia <ahmedalbashzia@gmail.com>
2026-05-26 13:40:11 +0500

    Adjusting preference order issue in move function logic

--- Code/Multiple Animations/Ball.py
+++ Code/Multiple Animations/Ball.py
@@ -8,10 +8,10 @@ class Ball:
     def move(self):
         coordinates = self.canvas.coords(self.image)
 
-        if (coordinates[2]>=self.canvas.winfo_width or coordinates[0]<0):
+        if (coordinates[2]>=(self.canvas.winfo_width()) or coordinates[0]<0):
             self.xVelocity = -self.xVelocity
 
-        if (coordinates[3]>=self.canvas.winfo_height or coordinates[1]<0):
+        if (coordinates[3]>=(self.canvas.winfo_height()) or coordinates[1]<0):
             self.yVelocity = -self.yVelocity
 
         self.canvas.move(self.image,self.xVelocity,self.yVelocity)
