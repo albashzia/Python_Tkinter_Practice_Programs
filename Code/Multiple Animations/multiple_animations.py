e24d3f267e2b603625b17b1a519dc37399872ac2
albashzia <ahmedalbashzia@gmail.com>
2026-05-26 13:44:19 +0500

    Creating Ball objects on canvas and calling move function of Ball class for them to show them moving on the canvas

--- Code/Multiple Animations/multiple_animations.py
+++ Code/Multiple Animations/multiple_animations.py
@@ -10,5 +10,16 @@ HEIGHT = 500
 canvas = Canvas(window, width= WIDTH, height= HEIGHT)
 canvas.pack()
 
+volley_ball = Ball(canvas,0,0,100,5,2,"white")
+tennis_ball = Ball(canvas,0,0,50,4,5,"red")
+basket_ball = Ball(canvas,0,0,125,8,6,"orange")
+
+
+while True:
+    volley_ball.move()
+    tennis_ball.move()
+    basket_ball.move()
+    window.update()
+    time.sleep(0.01)
 
 window.mainloop()
