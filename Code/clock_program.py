06ebaf3082e9e9a37624e18cd48c1eeb3a19b178
albashzia <ahmedalbashzia@gmail.com>
2026-05-27 22:42:54 +0500

    Creating an update function and defining the time_label data inside the function

--- Code/clock_program.py
+++ Code/clock_program.py
@@ -1,9 +1,15 @@
 from tkinter import *
 from time import *
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+
 
 window = Tk()
 
 time_label = Label(window,font=("Arial",50),bg="black",fg="green")
 time_label.pack()
+
+update()
 
 window.mainloop()
